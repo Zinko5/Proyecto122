8adbe8c802c460fb9bbacb5a92a62afa2607b365
Zinko <gabriel.marcelo.munoz@gmail.com>
2023-11-16 19:23:37 -0400

    modifica el login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function verifica(){
     let us= document.getElementById('us').value;
     let pa= document.getElementById('pass').value;
     if(us!="" && pa!=""){
-        window.location.href= "index2.html";
+        window.location.href= "index.html";
     }
     else
         alert("Llena todos los campos");
